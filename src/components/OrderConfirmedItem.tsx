9add1278d5a6501c624809500a68c368b809f15f
simplyObare <josephatowiny@gmail.com>
2024-12-21 03:22:17 +0300

    thumbnail image update

--- src/components/OrderConfirmedItem.tsx
+++ src/components/OrderConfirmedItem.tsx
@@ -9,8 +9,8 @@ const OrderConfirmedItem: React.FC<OrderConfirmedItemProps> = ({ cart }) => {
     <div className="main-container text-gray-700">
       <div className="my-4 flex items-center justify-between">
         <div className="img-and-name flex items-center gap-5">
-          <div className="img-container overflow-hidden w-14 h-14 rounded-xl">
-            <img src={cart.image} alt="" className="object-cover" />
+          <div className="img-container rounded-lg">
+            <img src={cart.image} alt="" className="object-cover rounded-lg" />
           </div>
           <div className="name-container flex flex-col justify-between">
             <h1 className="font-semibold">{cart.name}</h1>
